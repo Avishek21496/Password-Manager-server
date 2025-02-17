3664d223fbb5d48ef7e1216862f5de0fd1709dac
ashiqur12315 <ashiqur12315@gmail.com>
2025-02-18 02:02:38 +0600

    Final api structure

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-// Avishek Roy JbDmmHI7BojprzkP
+// AvishekRoy JbDmmHI7BojprzkP
 
 
 
@@ -30,112 +30,51 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-
-        //created a collection named 'items'
+        const passwordCollection = client.db('Password_Manager').collection('password');
         const itemCollection = client.db('itemsDB').collection('items');
-        const passwordCollection = client.db('itemsDB').collection('passwords');
-        //created a collection named 'categories' under same database
-        const categoriesCollection = client.db('itemsDB').collection('categories');
 
-        //added post method for inserting data
-        app.post('/items', async (req, res) => {
+        app.post('/saveCredentials', async (req, res) => {
             const addedItem = req.body;
-            // console.log(addedItem);
-            const result = await itemCollection.insertOne(addedItem);
-            res.send(result)
-        });
-
-        //get all the data
-        app.get('/items', async (req, res) => {
-            const cursor = itemCollection.find();
-            const result = await cursor.toArray();
-            res.send(result)
-        })
-
-        app.post('/save', async (req, res) => {
-            const addedItem = req.body;
-            console.log('----------------------HITTED--------------------');
             const result = await passwordCollection.insertOne(addedItem);
             res.send(result)
         })
-        app.get('/save/:email', async (req, res) => {
+        app.get('/myCredentials/:email', async (req, res) => {
             const email = req.params.email;
-            console.log(email)
+            console.log('email', email)
             const query = { user_email: email }
             const result = await passwordCollection.find(query).toArray()
             res.send(result)
         })
 
-        app.get('test', (req, res)=>{
-            console.log('test hit')
-        })
-
-        // access data by user id
-        app.get('/items/:id', async (req, res) => {
-            const ida = req.params.id;
-            const query = { _id: new ObjectId(ida) }
-            const result = await itemCollection.findOne(query)
-            console.log(result)
-            res.send(result)
-        })
-        //access logged in user crafts
-        app.get('/myItems/:email', async (req, res) => {
-            const email = req.params.email;
-            console.log(email)
-            const query = { user_email: email }
-            const result = await itemCollection.find(query).toArray()
-            res.send(result)
-        })
-        //added filter items
-        app.get('/myItems/filter/:email/:fil', async (req, res) => {
-            const email = req.params.email;
-            const fil = req.params.fil;
-            // console.log(email, fil)
-            const query = { user_email: email, customization: fil }
-            const result = await itemCollection.find(query).toArray()
+        app.get('/selectedPlatform/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await passwordCollection.findOne(query)
+            console.log('result', result)
             res.send(result)
         })
 
-        app.get('/categories', async(req, res)=>{
-            const cursor = categoriesCollection.find()
-            const result = await cursor.toArray()
-            res.send(result)
-        })
-        app.get('/subCategory/:subCategory', async(req, res)=>{
-            const subCategory = req.params.subCategory;
-            const query = {subcategory_Name : subCategory}
-            const cursor = itemCollection.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
-        })
-
-        app.put('/items/:id', async (req, res) => {
+        app.put('/updateCredentials/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) }
             const options = { upsert: true }
-            const updateItem = req.body;
+            const updateInfo = req.body;
             const item = {
                 $set: {
-                    image: updateItem.image,
-                    item_name: updateItem.item_name,
-                    subcategory_Name: updateItem.subcategory_Name,
-                    price: updateItem.price,
-                    short_description: updateItem.short_description,
-                    rating: updateItem.rating,
-                    customization: updateItem.customization,
-                    processing_time: updateItem.processing_time,
-                    stock_status: updateItem.stock_status
+                    platform_name: updateInfo.platform_name,
+                    platform_owner: updateInfo.platform_owner,
+                    platform_email: updateInfo.platform_email,
+                    platform_password: updateInfo.platform_password
                 }
             }
-            const result = await itemCollection.updateOne(filter, item, options)
+            const result = await passwordCollection.updateOne(filter, item, options)
             res.send(result)
         })
 
-        app.delete('/myItems/:id', async(req, res)=>{
+        app.delete('/deletePlatformCredentials/:id', async(req, res)=>{
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
-            const result = await itemCollection.deleteOne(query)
+            const result = await passwordCollection.deleteOne(query)
             res.send(result)
         })
 
